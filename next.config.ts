6f377b3bbdfb2fdec4848944a89fca83adbeb600
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-10-07 17:01:36 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-if (process.argv.includes("dev")) {
-  import("@opennextjs/cloudflare").then(({ initOpenNextCloudflareForDev }) => {
-    initOpenNextCloudflareForDev();
-  });
-}
 
 export default nextConfig;
